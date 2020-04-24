f9c7bd50221b5fd1a25890071cb80ac888cfcd7b
Alejandro Hernandez <alexxh42@gmail.com>
2020-04-24 00:42:58 -0500

    AutoAdjust

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       <input type="number" v-model="gridElement.percent" @blur="validatePercent(gridElement.id)">
     </div>
   </div>
-</template>
+</template> 
 
 <script>
 export default {
@@ -30,32 +30,72 @@ export default {
       virtual_grid.push(virtual_grid_selected_element);
 
       let global_current_percent = 0;
-      //console.log(virtual_grid);
+      let adjustable_overflow = 0;
 
       virtual_grid.map((gridElement, key) => {
         let temporal_current_percent = global_current_percent;
-        let overflow = 0;
         temporal_current_percent += parseInt(gridElement.percent, 0);
         console.log(temporal_current_percent, this.global_max_percent);
-        if (temporal_current_percent > this.global_max_percent) {
-          overflow = this.global_max_percent - global_current_percent;
-          console.log(`Te pasaste por ${overflow}, piensalo mejor!`);
-          gridElement.percent = overflow;
+
+        let overflow = temporal_current_percent - this.global_max_percent;
+        if (
+          temporal_current_percent > this.global_max_percent &&
+          !this.auto_adjust
+        ) {
+          console.log(
+            `Te pasaste por ${overflow}, Ajustando este ultimo valor!`
+          );
+          gridElement.percent =
+            this.global_max_percent - global_current_percent;
         } else {
+          if (
+            temporal_current_percent > this.global_max_percent &&
+            this.auto_adjust
+          ) {
+            console.log(
+              `Te pasaste por ${overflow}, Ajustando todos los valores a conveniencia de este ultimo valor!`
+            );
+            adjustable_overflow = overflow;
+          }
           global_current_percent = temporal_current_percent;
         }
         return gridElement;
       });
       virtual_grid[index].percent = virtual_grid.pop().percent;
-      //this.grid = virtual_grid;
+
+      if (this.auto_adjust && adjustable_overflow > 0) {
+        this.adjustOverflow(adjustable_overflow, index);
+      }
       console.log("_________");
       return virtual_grid[index];
     },
-    calculatePercent() {}
+    adjustOverflow(overflow, to_exclude_index) {
+      let unit_adjust = Math.floor(
+        overflow /
+          this.grid.filter(ge => ge.percent > 0 && ge.id !== to_exclude_index)
+            .length
+      );
+      let mod_adjust =
+        overflow %
+        this.grid.filter(ge => ge.percent > 0 && ge.id !== to_exclude_index)
+          .length;
+
+      console.log(
+        "Adjusting percents: " + unit_adjust + ", mod: " + mod_adjust
+      );
+      this.grid
+        .filter(ge => ge.percent > 0 && ge.id !== to_exclude_index)
+        .map((gridElement, index) => {
+          gridElement.percent -= unit_adjust;
+          if (index < mod_adjust) gridElement.percent--;
+          return gridElement;
+        });
+    }
   },
   data() {
     return {
       global_max_percent: 100,
+      auto_adjust: true,
       proto: {
         id: 0,
         percent: 0
